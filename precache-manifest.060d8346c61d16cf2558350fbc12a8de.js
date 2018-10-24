7e31cb0fb74edb79fdfc27b40337977be8b3938a
mhammedba <m-hammed.bakkouch@student.ecp.fr>
2018-10-24 12:45:22 +0200

    Updates

---
+++ precache-manifest.060d8346c61d16cf2558350fbc12a8de.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "79f6b6a0c4f3cca321dc458fe7e6544a",
+    "url": "/blindtest-spotify/static/media/logo.79f6b6a0.svg"
+  },
+  {
+    "revision": "9d4cbf40dcdac09bda6fa0aff397246b",
+    "url": "/blindtest-spotify/static/media/loading.9d4cbf40.svg"
+  },
+  {
+    "revision": "bd4de7ae32d74b318af5",
+    "url": "/blindtest-spotify/static/js/runtime~main.bd4de7ae.js"
+  },
+  {
+    "revision": "402766550ab7f9f60c76",
+    "url": "/blindtest-spotify/static/js/main.40276655.chunk.js"
+  },
+  {
+    "revision": "3f7525847cbf7fd2fe0c",
+    "url": "/blindtest-spotify/static/js/1.3f752584.chunk.js"
+  },
+  {
+    "revision": "402766550ab7f9f60c76",
+    "url": "/blindtest-spotify/static/css/main.bb44f8c9.chunk.css"
+  },
+  {
+    "revision": "4979c9b2f34153e21db8d8a1f429aa1e",
+    "url": "/blindtest-spotify/index.html"
+  }
+];
